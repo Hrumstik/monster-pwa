91944fda5fd56378f204e9f7d6cbf3d3c70c4eba
Aliaksandr Hurynovich <aliaksandr@atomatiq.io>
2024-12-09 18:56:02 +0100

    changed sidebar

--- src/core/Sidebar/MenuItem.tsx
+++ src/core/Sidebar/MenuItem.tsx
@@ -1,11 +1,36 @@
-const MenuItem = ({ text, onClick }: { text: string; onClick: () => void }) => {
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+
+const MenuItem = ({
+  text,
+  onClick,
+  path,
+  icon,
+}: {
+  text: string;
+  onClick: () => void;
+  path?: string;
+  icon?: React.ReactNode;
+}) => {
+  const location = useLocation();
+
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    setIsActive(location.pathname === path);
+  }, [location.pathname, path]);
+
   return (
     <div
       onClick={onClick}
-      className="bg-[#0E421D] hover:bg-[#20223B] h-[42px] rounded flex items-center p-[14px] cursor-pointer"
+      className={`${
+        isActive ? "bg-[#0d421d]" : ""
+      } group hover:bg-[#20223B] h-[42px] rounded flex items-center p-[14px] cursor-pointer gap-[14px]`}
     >
-      <div className="w-[10px] h-[10px] rounded-full bg-[#00FF11] mr-[14px]" />
-      <span className="text-base text-[#00FF11] leading-5">{text}</span>
+      {icon ?? <div className="w-[10px] h-[10px] rounded-full bg-[#00FF11]" />}
+      <span className="text-base text-[#00FF11] group-hover:text-white leading-5">
+        {text}
+      </span>
     </div>
   );
 };
--- src/core/Sidebar/Sidebar.tsx
+++ src/core/Sidebar/Sidebar.tsx
@@ -1,5 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import MenuItem from "./MenuItem";
+import DocumentationIcon from "@icons/Documentation";
+import { IoLogoPwa } from "react-icons/io5";
 
 const Sidebar = () => {
   const navigate = useNavigate();
@@ -8,7 +10,31 @@ const Sidebar = () => {
       <div className="flex justify-center font-normal font-reemkufi text-[18px] text-[#00FF11] leading-[27px] mb-[95px]">
         Monster PWA
       </div>
-      <MenuItem text="Мои PWA" onClick={() => navigate("/")} />
+      <div className="flex flex-col gap-2.5">
+        <MenuItem
+          text="Мои PWA"
+          onClick={() => navigate("/")}
+          path="/"
+          icon={
+            <IoLogoPwa
+              color="white"
+              style={{
+                fontSize: "28px",
+              }}
+            />
+          }
+        />
+        <MenuItem
+          icon={<DocumentationIcon />}
+          text="Документация"
+          onClick={() =>
+            window.open(
+              "https://vibegamesteam.notion.site/PWA-Monster-506fc22fe0ec4b99a2cb47420a17e324",
+              "_blank"
+            )
+          }
+        />
+      </div>
     </div>
   );
 };
