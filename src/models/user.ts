3782b8ecbcda669c9043cdd3688472903b74d9ba
Aliaksandr Hurynovich <aliaksandr@atomatiq.io>
2024-12-29 19:36:52 +0100

    fixed types

--- src/models/user.ts
+++ src/models/user.ts
@@ -15,6 +15,7 @@ export interface User {
   email: string;
   __v: 0;
   updatedAt: string;
+  cfAccounts: { email: string; gApiKey: string }[];
   pwas: {
     domainName?: string;
     pwaContentId: string;
